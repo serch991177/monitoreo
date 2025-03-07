694b4659621e73da81e2568100e3b7cd513c4f0c
unknown <sergioandia11@gmail.com>
2025-03-07 15:48:12 -0400

    cambio de baseurl y fix bug en libreria

--- application/config/config.php
+++ application/config/config.php
@@ -27,7 +27,8 @@ date_default_timezone_set('America/La_Paz');
 /*$root = "http://".$_SERVER['HTTP_HOST'];
 $root .= str_replace(basename($_SERVER['SCRIPT_NAME']),"",$_SERVER['SCRIPT_NAME']);
 $config['base_url']    = "$root";*/
-$config['base_url'] = 'https://monitoreo.cochabamba.bo';
+//$config['base_url'] = 'https://monitoreo.cochabamba.bo';
+$config['base_url'] = 'http://localhost:8080/monitoreo/';
 //$config['base_url'] = 'http://localhost/atencion-al-ciudadano';
 
 /*
